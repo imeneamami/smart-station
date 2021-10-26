4f0c18a9554b3917f43d1283d97e1eaa7c1e5f6b
emnagharbi <emna.gharbi1@esprit.tn>
2021-10-26 07:28:38 +0200

    mon projet avec ajout supression

---
+++ smartstation2A19/mainwindow.cpp
@@ -0,0 +1,61 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "voyage.h"
+#include "connexion.h"
+#include <QIntValidator>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->le_id->setValidator(new QIntValidator(100,999999,this));
+    ui->ta_voyage->setModel(Etmp.afficher());
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pb_ajouter_clicked()
+{
+    int idvoyage=ui->le_id->text().toInt();
+    int numtrain=ui->le_numtrain->text().toInt();
+    QString arretd=ui->le_arretd->text();
+    QString arretf=ui->le_arretf->text();
+    int nbrplace=ui->le_nbrplace->text().toInt();
+    QString heured=ui->le_heured->text();
+    Voyage V(idvoyage,numtrain,arretd,arretf,nbrplace,heured);
+    bool test=V.ajouter();
+
+    if (test)
+
+    {
+
+        QMessageBox::information(nullptr, QObject::tr("ajout voyage"),
+        QObject::tr("voyage ajouté :"), QMessageBox::Cancel);
+    }
+
+}
+
+void MainWindow::on_pb_supprimer_clicked()
+{
+
+    Voyage V1;
+    V1.setidvoyage(ui->le_idsuprimer->text().toUInt());
+    bool test=V1.supprimer(V1.getidvoyage());
+
+    if (test)
+     {
+       ui->ta_voyage->setModel(V1.afficher());
+
+      QMessageBox::information(nullptr, QObject::tr(" suppression voyage "),
+                             QObject::tr("voyage supprimé :"), QMessageBox::Cancel);
+     }
+
+}
---
+++ smartstation2A19/voyage.h
@@ -0,0 +1,53 @@
+#ifndef VOYAGE_H
+#define VOYAGE_H
+#include <QString>
+#include <QDate>
+#include <QSqlQueryModel>
+#include <QSqlQuery>
+#include <QSqlDatabase>
+#include "connexion.h"
+#include <QDebug>
+#include <QFileDialog>
+#include <QtDebug>
+#include <QMessageBox>
+#include <QDateTime>
+
+
+class Voyage
+{
+
+public:
+    Voyage();
+    Voyage(int,int,QString,QString,int,QString);
+    int getidvoyage();
+    int getnumtrain();
+    QString getarretd();
+    QString getarretf();
+     int getnbrplace();
+    QString getheure();
+
+    void setidvoyage(int);
+     void setnumtrain(int);
+     void setarretd(QString);
+     void setarretf(QString);
+     void setnbrplace(int);
+     void setheure(QString);
+
+  QSqlQueryModel * afficher();
+
+    bool ajouter();
+    bool modifier();
+    bool supprimer(int);
+
+private:
+   int idvoyage;
+    int numtrain;
+    QString arretd;
+    QString arretf;
+    int nbrplace;
+    QString heured;
+
+
+};
+
+#endif // VOYAGE_H
