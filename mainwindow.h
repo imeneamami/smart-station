feaf9b23eaf13fd9685e7db6c92ce79fd456f155
imen <80150792+imeneamami@users.noreply.github.com>
2021-11-07 22:49:38 +0000

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,11 +1,12 @@
    #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "employes.h"
 
 #include <QMainWindow>
+QT_BEGIN_NAMESPACE
 
-namespace Ui {
-class MainWindow;
-}
+namespace Ui {class MainWindow;}
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
@@ -16,10 +17,25 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_4_clicked();
+
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+    void on_pb_modifier_clicked();
+
+
+    void on_pb_se_connecter_clicked();
+
+    void on_le_mailing_clicked();
+
+    void on_pb_envoyer_clicked();
+
+    void on_pb_supprimer_2_clicked();
 
 private:
     Ui::MainWindow *ui;
+    Employes E;
 };
 
 #endif // MAINWINDOW_H
