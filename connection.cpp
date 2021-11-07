e46b7f40ad693f61be39e69808d4cb98ba757d32
imen <80150792+imeneamami@users.noreply.github.com>
2021-11-07 22:47:01 +0000

    Update connection.cpp

--- connection.cpp
+++ connection.cpp
@@ -6,18 +6,16 @@ Connection::Connection()
 }
 
 bool Connection::createconnect()
-{bool test=false;
+{
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("projet");//inserer le nom de la source de données ODBC
-db.setUserName("imen");//inserer nom de l'utilisateur
-db.setPassword("imen");//inserer mot de passe de cet utilisateur
+
+
+db.setDatabaseName("smart");//inserer le nom de la source de données ODBC
+db.setUserName("test");//inserer nom de l'utilisateur
+db.setPassword("imen123");//inserer mot de passe de cet utilisateur
 
 if (db.open())
-test=true;
+    return true;
 
-
-
-
-
-    return  test;
 }
+
