431203be83805c994c20b4038181f3a6350695fc
imen <80150792+imeneamami@users.noreply.github.com>
2021-11-07 22:47:42 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QMessageBox>
+#include <QDebug>
 #include "connection.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     Connection c;
+
     bool test=c.createconnect();
     if(test)
-    {w.show();
+    {
+        w.show();
         QMessageBox::information(nullptr, QObject::tr("database is open"),
                     QObject::tr("connection successful.\n"
                                 "Click Cancel to exit."), QMessageBox::Cancel);
