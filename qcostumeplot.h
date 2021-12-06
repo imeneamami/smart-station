8a9ca4b275eabe765c650560fc30d413072e9644
imen <80150792+imeneamami@users.noreply.github.com>
2021-12-06 18:46:44 +0000

    Update qcostumeplot.h

--- qcostumeplot.h
+++ qcostumeplot.h
@@ -1,6 +1,6 @@
 /***************************************************************************
 **                                                                        **
-**  QCustomPlot, an easy to use, modern plotting widget for Qt            **
+**  QCustomPlot, an easy to use, modern plotting widget for Qt            **   
 **  Copyright (C) 2011-2017 Emanuel Eichhammer                            **
 **                                                                        **
 **  This program is free software: you can redistribute it and/or modify  **
